2da7a070bcafa760e973220958a1e809fd89aa60
RobertJuergens <robert.juergens@outlook.com>
2014-12-04 17:57:24 +0000

    buttons added to history page to switch between graph/table/summary

---
+++ nhs-nutrition-diary/WebContent/scripts/historyViewer.js
@@ -0,0 +1,6 @@
+function toggleVisualisation() {
+//	var presentationFormat = alert($('#presentationFormat').val());
+	var presentedParameter = $('#foodComponents').val();
+	var dateFrom = $('#datepickerFrom').val();
+	var dateTo = $('#datepickerTo').val();
+}

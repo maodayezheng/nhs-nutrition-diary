f386ecb4d93db7a4aa553dd3a3527306fe891ca8
RobertJuergens <robert.juergens@outlook.com>
2014-12-05 21:40:17 +0000

    database object created in submit controller

--- nhs-nutrition-diary/WebContent/scripts/SubmitController.js
+++ nhs-nutrition-diary/WebContent/scripts/SubmitController.js
@@ -2,6 +2,10 @@ function SubmitController() {}
 
 SubmitController.prototype.submit = function(submitter) {
 	if(submitter == "btn_submit_symptoms") {
-		//TODO
+		var database = new LocalDbSingleton();
+		
+		
+		//TODO retrieve data that needs to be sent to server
+		//TODO submit data
 	}
 }

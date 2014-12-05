790e98010125d1b1fe3169c26174ae8dd9c6c354
User <vab2048@gmail.com>
2014-12-05 22:36:50 +0000

    wrote example submit function in submit controller

--- nhs-nutrition-diary/WebContent/scripts/SubmitController.js
+++ nhs-nutrition-diary/WebContent/scripts/SubmitController.js
@@ -4,6 +4,8 @@ SubmitController.prototype.submit = function(submitter) {
 	if(submitter == "btn_submit_symptoms") {
 		var database = new LocalDbSingleton();
 		
+		//database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
+		
 		
 		//TODO retrieve data that needs to be sent to server
 		//TODO submit data

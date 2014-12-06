d38844f1b88dea1cfcc738d4ca5ffeb4df46e4b2
RobertJuergens <robert.juergens@outlook.com>
2014-12-06 00:28:54 +0000

    test function improved

--- nhs-nutrition-diary/WebContent/scripts/SubmitController.js
+++ nhs-nutrition-diary/WebContent/scripts/SubmitController.js
@@ -2,13 +2,12 @@ function SubmitController() {}
 
 SubmitController.prototype.submit = function(submitter) {
 	if(submitter == "btn_submit_symptoms") {
-		alert('button is clicked');
+		console.log('Submit button clicked. Transaction is prepared.');
 		
 		var database = new LocalDbSingleton();
-		var arrayToAdd = [{name: "one"},{name: "two"},{name: "three"},{name: "four"}];
-		database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
-		
-		//TODO retrieve data that needs to be sent to server
 		//TODO submit data
+		var arrayToAdd = [{name: "one"},{name: "two"},{name: "three"},{name: "four"}];
+		//TODO retrieve data that needs to be sent to server
+		database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
 	}
 }

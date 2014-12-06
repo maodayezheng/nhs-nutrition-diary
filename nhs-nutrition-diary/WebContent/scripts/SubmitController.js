dff4a827b44b373a9cfd61c6e916a1c611c465f6
RobertJuergens <robert.juergens@outlook.com>
2014-12-06 00:37:20 +0000

    objects store changed. still problem in localDb.js

--- nhs-nutrition-diary/WebContent/scripts/SubmitController.js
+++ nhs-nutrition-diary/WebContent/scripts/SubmitController.js
@@ -5,9 +5,9 @@ SubmitController.prototype.submit = function(submitter) {
 		console.log('Submit button clicked. Transaction is prepared.');
 		
 		var database = new LocalDbSingleton();
-		//TODO submit data
-		var arrayToAdd = [{name: "one"},{name: "two"},{name: "three"},{name: "four"}];
 		//TODO retrieve data that needs to be sent to server
-		database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
+		var arrayToAdd = [{name: "one"},{name: "two"},{name: "three"},{name: "four"}];
+		//TODO submit data
+		database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'symptomManifestStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
 	}
 }

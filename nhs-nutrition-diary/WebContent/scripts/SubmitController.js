2509d81a0f1d7bbc0e110cfc0eda3fe49776da4e
RobertJuergens <robert.juergens@outlook.com>
2014-12-06 00:13:39 +0000

    test database add function in submit controller implemented

--- nhs-nutrition-diary/WebContent/scripts/SubmitController.js
+++ nhs-nutrition-diary/WebContent/scripts/SubmitController.js
@@ -4,8 +4,9 @@ SubmitController.prototype.submit = function(submitter) {
 	if(submitter == "btn_submit_symptoms") {
 		alert('button is clicked');
 		
-		//var database = new LocalDbSingleton();
-		//database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
+		var database = new LocalDbSingleton();
+		var arrayToAdd = [{name: "one"},{name: "two"},{name: "three"},{name: "four"}];
+		database.databaseOpen(LocalDbSingleton.prototype.localDbAdd, 'userFoodListStore', arrayToAdd); //example code to add an array of objects to the specified object store (arg2) 
 		
 		//TODO retrieve data that needs to be sent to server
 		//TODO submit data

749dc45aa1506d3d7bdb14ae8f07a193592cc2f4
RobertJuergens <robert.juergens@outlook.com>
2014-12-08 10:44:50 +0000

    todays balance renamed to 'home.html'

--- nhs-nutrition-diary/WebContent/scripts/OnLoad.js
+++ nhs-nutrition-diary/WebContent/scripts/OnLoad.js
@@ -2,7 +2,7 @@ function OnLoad() {}
 
 OnLoad.prototype.load = function(pageName) {
 	switch(pageName) {
-		case 'index': this.updateTodaysBalance(); break;
+		case 'home': this.updateTodaysBalance(); break;
 		case 'history': toggleHistoryVisualisation(); break;
 		case 'weight': this.updateWeight(); break;
 		default: console.log('pageName does not exist.');

49f7421ef1d258b223b086e74339d8abd558041f
jobarry <jobarry@tcd.ie>
2014-12-14 17:36:15 +0000

    Symptom Not on List Functionality (TODO: layout)

--- nhs-nutrition-diary/WebContent/scripts/symptoms.js
+++ nhs-nutrition-diary/WebContent/scripts/symptoms.js
@@ -1,71 +1,93 @@
 $(document).ready(function () {
+	
+/* This function creates the bootstrap style check boxes
+ * call "setUpCheckbox" on $('.list-group.checked-list-box .list-group-item') */
+	
+    var setUpCheckbox = function() {
+		
+		 $('.list-group.checked-list-box .list-group-item').each(function () {
+		        // Settings
+		        var $widget = $(this),
+		            $checkbox = $('<input type="checkbox" class="hidden"/>'),
+		            color = ($widget.data('color') ? $widget.data('color') : "primary"),
+		            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+		            settings = {
+		                on: {
+		                    icon: 'glyphicon glyphicon-check'
+		                },
+		                off: {
+		                    icon: 'glyphicon glyphicon-unchecked'
+		                }
+		            };
+		            
+		        $widget.css('cursor', 'pointer')
+		        $widget.append($checkbox);
+
+		        // Event Handlers
+		        $widget.on('click', function () {
+		            $checkbox.prop('checked', !$checkbox.is(':checked'));
+		            $checkbox.triggerHandler('change');
+		            updateDisplay();
+		        });
+		        $checkbox.on('change', function () {
+		            updateDisplay();
+		        });
+		          
+		        // Actions
+		        function updateDisplay() {
+		            var isChecked = $checkbox.is(':checked');
+
+		            // Set the button's state
+		            $widget.data('state', (isChecked) ? "on" : "off");
+
+		            // Set the button's icon
+		            $widget.find('.state-icon')
+		                .removeClass()
+		                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+		            // Update the button's color
+		            if (isChecked) {
+		                $widget.addClass(style + color + ' active');
+		            } else {
+		                $widget.removeClass(style + color + ' active');
+		            }
+		        }
+		        
+
+		        function init() {
+		            
+		            if ($widget.data('checked') == true) {
+		                $checkbox.prop('checked', !$checkbox.is(':checked'));
+		            }
+		            
+		            updateDisplay();
+
+		            // Inject the icon if applicable
+		            if ($widget.find('.state-icon').length == 0) {
+		                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+		            }
+		        }
+		        init();
+		    });
+	     };
+	     
+	/* "#saveNewSymptom" is used on the "symptomNotOnList page"
+	 * it is used when the user enters a custom symptom 
+	 * which is then appended to the $("#newSymptomList")
+	 * */
+	
+	$("#saveNewSymptom").click(function(){	
+		var symptom = $('#newSymptom').val();
+		var newSymptomInList = '<li class="list-group-item" style="cursor: pointer;"><span class="state-icon glyphicon glyphicon-unchecked"></span>'+symptom+'</li>';
+		$("#newSymptomList").append(newSymptomInList);	     
+	     setUpCheckbox();		
+	});
+
 
 	var symptoms = new SymptomListSingleton().symptomList;
 	ko.applyBindings(new symptomsListView(symptoms));
 
-   $('.list-group.checked-list-box .list-group-item').each(function () {
-        // Settings
-        var $widget = $(this),
-            $checkbox = $('<input type="checkbox" class="hidden"/>'),
-            color = ($widget.data('color') ? $widget.data('color') : "primary"),
-            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
-            settings = {
-                on: {
-                    icon: 'glyphicon glyphicon-check'
-                },
-                off: {
-                    icon: 'glyphicon glyphicon-unchecked'
-                }
-            };
-            
-        $widget.css('cursor', 'pointer')
-        $widget.append($checkbox);
-
-        // Event Handlers
-        $widget.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-            $checkbox.triggerHandler('change');
-            updateDisplay();
-        });
-        $checkbox.on('change', function () {
-            updateDisplay();
-        });
-          
-        // Actions
-        function updateDisplay() {
-            var isChecked = $checkbox.is(':checked');
-
-            // Set the button's state
-            $widget.data('state', (isChecked) ? "on" : "off");
-
-            // Set the button's icon
-            $widget.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$widget.data('state')].icon);
-
-            // Update the button's color
-            if (isChecked) {
-                $widget.addClass(style + color + ' active');
-            } else {
-                $widget.removeClass(style + color + ' active');
-            }
-        }
-
-        function init() {
-            
-            if ($widget.data('checked') == true) {
-                $checkbox.prop('checked', !$checkbox.is(':checked'));
-            }
-            
-            updateDisplay();
-
-            // Inject the icon if applicable
-            if ($widget.find('.state-icon').length == 0) {
-                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
-            }
-        }
-        init();
-    });
+	setUpCheckbox();
     
    // $('#get-checked-data').on('click', function(event) {
        // event.preventDefault(); 
@@ -83,16 +105,24 @@ $(document).ready(function () {
        // $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
   //  });
 
-	$('li').click(function(){
-		var target = $(this).next(".drop-scoring");	
-	$(target).slideToggle('slow');
-	});
+
 
 
 	$("#saveWeight").click(function(){
 		$('p').html("#currentWeight");
 		//$("#currentweight").val("#number");
 	});
+	
+	
+
+	
+	$('li').click(function(){
+		var target = $(this).next(".drop-scoring");	
+	$(target).slideToggle('slow');
+	});
+
+	
+	
 });
 
 //---- method to combine data with DOMs----
@@ -122,6 +152,8 @@ function symptomsListView(symptoms){
 		},this);
 		
 }
+
+
 
 
 ////////////////////////////////NOT USED IN SYMPTOMS//////////////////////////////
@@ -193,3 +225,6 @@ $(function() {
         clearInterval(action);
     });
 });
+
+
+

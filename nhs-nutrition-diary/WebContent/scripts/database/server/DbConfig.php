99d04968a9589c1fe8344446743f88512ae3ac24
User <vab2048@gmail.com>
2014-12-17 12:42:06 +0000

    database is singleton

--- nhs-nutrition-diary/WebContent/scripts/database/server/DbConfig.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/DbConfig.php
@@ -1,6 +1,6 @@
 <?php
  
-class Dbconfig 
+class DbConfig 
 {
 	private $serverName;
 	private $userName;
--- nhs-nutrition-diary/WebContent/scripts/database/server/ServerDatabase.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/ServerDatabase.php
@@ -1,16 +1,34 @@
 <?php
-
-class ServerDatabase extends Dbconfig
+/**
+ * This class extends the DbConfig class allowing it to have access to its parents configuration variables.
+ * This class implements the singleton pattern to enforce only one connection to the database at a single time. To return 
+ * a connection to the database the user should use call the static method 'ServerDatabase::getInstance();'
+ * Created: 16th December 2014. 
+ * @author Vikram Bakshi, Robert Juergens 
+ */
+class ServerDatabase extends DbConfig
 {
-	private $db;
+	private static $_instance = null; //instance for the singleton. 
+	private $db, $_query, $_error=false, $_results, $_count = 0; 
 	
-	public function __construct()
+	private function __construct()
 	{
 		echo "\nin constructor of ServerDatabase\n";
 		parent::__construct(); 
 		$this -> db = new mysqli(parent::getServerName(), parent::getUserName(), parent::getPassCode(), parent::getDbName()); // Create connection
-		if ($this->db->connect_error) { die("Connection failed: " . $this->db); } // Check connection
-		
+		if ($this->db->connect_error) { die("Connection failed: " . $this->db); } // Check connection was successful. 
+	}
+	
+	public static function getInstance()
+	{
+		if (!isset(self::$_instance))
+		{
+			self::$_instance = new ServerDatabase();
+		}
+		else 
+		{
+			return self::$_instance; 
+		}
 	}
 	
 	public function retrieveData()
@@ -22,7 +40,6 @@ class ServerDatabase extends Dbconfig
 		} catch (Exception $e)
 		{
 			echo $e->getMessage();
-			return;
 		}
 	}
 	
@@ -33,7 +50,8 @@ class ServerDatabase extends Dbconfig
 		echo "\nconnection closed\n";
 	}
 	
-	public function getDb() {
+	public function getDb() 
+	{
 		return $this -> db;
 	}
 	

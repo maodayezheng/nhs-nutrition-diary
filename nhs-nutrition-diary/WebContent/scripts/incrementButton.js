d2526a1fe7d71424805d897707f5678d48e70b39
jobarry <jobarry@tcd.ie>
2014-12-27 01:45:07 +0000

    Ammend requirements (in settings) update requirements complete

--- nhs-nutrition-diary/WebContent/scripts/incrementButton.js
+++ nhs-nutrition-diary/WebContent/scripts/incrementButton.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(function(){
+	$("#btn_submit_settings").click(function(){
+			
+	//TODO replace hard-coded value with same db getters as onLoad.js
+	var caloriesRequirement = 2000;
+	var proteinRequirement = 45;
+	var fluidRequirement = 3150;
+	var activityLevel = 1;
 	
+	// Value of decrease/increase adjustments by dietician	
+	var calsToAdjust = parseInt($("#cals").val());
+	var proteinToAdjust = parseInt($("#protein").val());
+	var fluidToAdjust = parseInt($("#fluid").val());
+	var activityToAdjust = parseFloat($("#activity").val());
+
+	$("#currentCals").html(calsToAdjust+ caloriesRequirement);
+	$("#currentProtein").html(proteinToAdjust+ proteinRequirement);
+	$("#currentFluid").html(fluidToAdjust+ fluidRequirement);
+	$("#currentActivity").html(activityToAdjust+ activityLevel);
+	});
 	
     $('.qtyPlusCal').click(function(e){
         e.preventDefault();

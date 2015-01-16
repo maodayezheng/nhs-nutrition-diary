a7f69d9e21cf09c736555b0007f5dc68b51de311
jobarry <jobarry@tcd.ie>
2015-01-16 00:59:54 +0000

    email mock fields removed, functionality transfered to export button history page

--- nhs-nutrition-diary/WebContent/scripts/database/server/contact-form-handler.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/contact-form-handler.php
@@ -1,18 +1,16 @@
 
 <?php
-$first_name=$_POST['first_name'];
-$last_name=$_POST['last_name'];
+//$first_name=$_POST['first_name'];
+//$last_name=$_POST['last_name'];
 $email= $_POST['email'];
-$message= $_POST['message'];
-
+$message= "A report sent via Appetite";
 
 $subject="GST Patient Report";
 $headers .= 'From: Patient at GST <PatientGST@gmail.com>' . "\r\n";
 
-
 // Mail it
 mail($email, $subject, $message, $headers);
-echo "Your message has been sent bro";
+echo "Your message has been sent";
 
 ?>
 

43794c095f856422163f020debd8019730d6877c
jobarry <jobarry@tcd.ie>
2015-01-17 11:57:01 +0000

    phpmailer library added to server, works way better than reg php mail()

--- nhs-nutrition-diary/WebContent/scripts/database/server/contact-form-handler.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/contact-form-handler.php
@@ -1,16 +1,41 @@
 
 <?php
-//$first_name=$_POST['first_name'];
-//$last_name=$_POST['last_name'];
-$email= $_POST['email'];
-$message= "A report sent via Appetite";
+require 'phpmailer/PHPMailerAutoload.php';
 
-$subject="GSST Patient Report";
-$headers .= 'From: Patient at GSST <PatientGSST@gmail.com>' . "\r\n";
+$mail = new PHPMailer;
 
-// Mail it
-mail($email, $subject, $message, $headers);
-echo "Your message has been sent";
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'patientgsst@gmail.com';                 // SMTP username
+$mail->Password = 'group15nhs';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->From = 'from@example.com';
+$mail->FromName = 'Mailer';
+$mail->addAddress('jobarry@tcd.ie', 'Joe User');     // Add a recipient
+$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo('info@example.com', 'Information');
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');
+
+$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 
 ?>
 

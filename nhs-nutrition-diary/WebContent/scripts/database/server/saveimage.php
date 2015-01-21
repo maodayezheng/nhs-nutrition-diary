26934e543e5c3ba072a782fd4c7ef3d20ab1ff19
jobarry <jobarry@tcd.ie>
2015-01-21 13:27:45 +0000

    echo exec('whoami'); php (for testing images db config)

--- nhs-nutrition-diary/WebContent/scripts/database/server/saveimage.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/saveimage.php
@@ -4,6 +4,7 @@
 //Show the image
 $image='<img src="'.$_POST['img_val'].'" />';
 echo $image;
+echo exec('whoami'); 
  
 //Get the base-64 string from data
 $filteredData=substr($_POST['img_val'], strpos($_POST['img_val'], ",")+1);

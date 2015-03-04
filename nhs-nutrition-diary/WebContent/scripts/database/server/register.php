837653825e192064683a037fcac53a8605117b8b
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2015-03-04 13:31:37 +0000

    update test

--- nhs-nutrition-diary/WebContent/scripts/database/server/register.php
+++ nhs-nutrition-diary/WebContent/scripts/database/server/register.php
@@ -6,9 +6,8 @@
  * @author Vikram Bakshi
  * */
 
-
-require_once 'init.php';
 echo "I am here";
+require_once 'init.php';
 if($data = Input::retrieveData()) 
 {
 	$dataDecoded = json_decode($data, true); //decode the json data with the true flag so that the object is converted into an associative array. 

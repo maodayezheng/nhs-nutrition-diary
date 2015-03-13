0dc5aec41c1df126b1b366342eb7758d091c9412
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2015-03-13 07:40:53 +0000

    update js

--- nhs-nutrition-diary/WebContent/scripts/Navigator.js
+++ nhs-nutrition-diary/WebContent/scripts/Navigator.js
@@ -5,15 +5,15 @@ Navigator.prototype.navigate = function(origin) {
 	console.log("signUp");
 	switch(origin)
 	{		 
-		case "btn_signin_to_home": nextPage = 'nhs-nutrition-diary/WebContent/home.html'; break;
-		case "btn_signup_to_details": nextPage = 'nhs-nutrition-diary/WebContent/signUpDetails.html'; break;
-		case "btn_signUpDetails_to_home": nextPage = 'nhs-nutrition-diary/WebContent/home.html'; break;
-		case "btn_home_to_food": nextPage = 'nhs-nutrition-diary/WebContent/food.html'; break;
-		case "btn_home_to_symptoms": nextPage = 'nhs-nutrition-diary/WebContent/symptoms.html'; break;
-		case "btn_home_to_history": nextPage = 'nhs-nutrition-diary/WebContent/history.html'; break
-		case "btn_home_to_weight": nextPage = 'nhs-nutrition-diary/WebContent/weight.html'; break;
-		case "listEl_home_to_settings": nextPage = 'nhs-nutrition-diary/WebContent/settings.html'; break;
-		case "btn_symptoms_to_symptomNotInList": nextPage = 'nhs-nutrition-diary/WebContent/symptomNotInList.html'; break;
+		case "btn_signin_to_home": nextPage = 'home.html'; break;
+		case "btn_signup_to_details": nextPage = 'signUpDetails.html'; break;
+		case "btn_signUpDetails_to_home": nextPage = 'home.html'; break;
+		case "btn_home_to_food": nextPage = 'food.html'; break;
+		case "btn_home_to_symptoms": nextPage = 'symptoms.html'; break;
+		case "btn_home_to_history": nextPage = 'history.html'; break
+		case "btn_home_to_weight": nextPage = 'weight.html'; break;
+		case "listEl_home_to_settings": nextPage = 'settings.html'; break;
+		case "btn_symptoms_to_symptomNotInList": nextPage = 'symptomNotInList.html'; break;
 		default: console.log('Hyperlink unknown.');
 	}
 	
@@ -21,7 +21,7 @@ Navigator.prototype.navigate = function(origin) {
 }
 Navigator.prototype.setting = function(){
 	
-	window.location.href = "nhs-nutrition-diary/WebContent/settingsPassword.html";
+	window.location.href = "settingsPassword.html";
 	
 }
 
